66e8f010912a427f62d65368a19aa450dda71168
v <xuantruongit32@gmail.com>
2024-03-10 09:25:40 +0700

    add updateFollowList

--- zoo/lib/network/fire_store.dart
+++ zoo/lib/network/fire_store.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:zoo/data/data.dart';
+import 'package:zoo/services/models/house.dart';
 
 class FireStore {
   String? getUserId() {
@@ -17,5 +19,22 @@ class FireStore {
     final userId = getUserId();
     CollectionReference collection = FirebaseFirestore.instance.collection('users/$userId/followList');
     await collection.add(id);
+  }
+
+  Future<void> updateFollowList() async {
+    try {
+      final userId = getUserId();
+
+      CollectionReference followListRef = FirebaseFirestore.instance.collection('users/$userId/followList');
+
+      for (House house in DataManager.followList) {
+        await followListRef.doc(house.id).set({'timestamp': FieldValue.serverTimestamp()});
+        print('House with ID ${house.id} added to followList');
+      }
+
+      print('All houses added to followList successfully');
+    } catch (e) {
+      print('Error adding houses to followList: $e');
+    }
   }
 }
