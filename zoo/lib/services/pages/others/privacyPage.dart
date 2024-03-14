4f6445b5f2098661c70afec2fd7eee5320724354
v <xuantruongit32@gmail.com>
2024-03-14 15:07:12 +0700

    add newName

--- zoo/lib/services/pages/others/privacyPage.dart
+++ zoo/lib/services/pages/others/privacyPage.dart
@@ -13,6 +13,19 @@ class _PrivacyPageState extends State<PrivacyPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String _newPassword = '';
   String _currentPassword = '';
+  String _newName = '';
+
+  String _getName() {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      if (user.displayName == null) {
+        return "No name";
+      } else {
+        return user.displayName!;
+      }
+    }
+    return '';
+  }
 
   Future<void> _changePassword() async {
     try {
@@ -36,79 +49,108 @@ class _PrivacyPageState extends State<PrivacyPage> {
     }
   }
 
+  Future<void> _changeName() async {
+    try {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        await user.updateDisplayName(_newName);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Name changed successfully')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to change name: $e')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Current Password',
-                  border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Current Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your current password';
+                    }
+                    return null;
+                  },
+                  onChanged: (String value) {
+                    setState(() {
+                      _currentPassword = value;
+                    });
+                  },
                 ),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your current password';
-                  }
-                  return null;
-                },
-                onChanged: (String value) {
-                  setState(() {
-                    _currentPassword = value;
-                  });
-                },
-              ),
-              const Gap(20),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'New Password',
-                  border: OutlineInputBorder(),
+                const Gap(20),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'New Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a new password';
+                    }
+                    return null;
+                  },
+                  onChanged: (String value) {
+                    setState(() {
+                      _newPassword = value;
+                    });
+                  },
                 ),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a new password';
-                  }
-                  return null;
-                },
-                onChanged: (String value) {
-                  setState(() {
-                    _newPassword = value;
-                  });
-                },
-              ),
-              const Gap(20),
-              AuthButton(
-                buttonText: 'Change Password',
-                fun: () {
-                  if (_formKey.currentState!.validate()) {
-                    _changePassword();
-                  }
-                },
-              ),
-              const Gap(50),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'New Name',
-                  border: OutlineInputBorder(),
+                const Gap(20),
+                AuthButton(
+                  buttonText: 'Change Password',
+                  fun: () {
+                    if (_formKey.currentState!.validate()) {
+                      _changePassword();
+                    }
+                  },
                 ),
-              ),
-              const Gap(20),
-              AuthButton(
-                buttonText: 'Change Name',
-                fun: () {
-                  _changePassword();
-                },
-              ),
-            ],
+                const Gap(50),
+                TextFormField(
+                  initialValue: _getName(),
+                  decoration: const InputDecoration(
+                    labelText: 'New Name',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a new name';
+                    }
+                    return null;
+                  },
+                  onChanged: (String value) {
+                    setState(() {
+                      _newName = value;
+                    });
+                  },
+                ),
+                const Gap(20),
+                AuthButton(
+                  buttonText: 'Change Name',
+                  fun: () {
+                    _changeName();
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
